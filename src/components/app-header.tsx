05ef59c6da0fe6195591c2e3ae6b00a6d7a71e37
Matt Taylor <matt@afterhoursds.com>
2025-09-09 15:15:50 +0000

    the logo is on top of the navigation

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -38,13 +38,15 @@ export default function AppHeader() {
             </SheetTrigger>
             <SheetContent side="left" className="sm:max-w-xs">
                 <nav className="grid gap-6 text-lg font-medium">
-                    <Link
-                        href="/admin"
-                        className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                    >
-                        <Volleyball className="h-5 w-5 transition-all group-hover:scale-110" />
-                        <span className="sr-only">Saeed's Shuffle</span>
-                    </Link>
+                    <div className="mb-4">
+                        <Link
+                            href="/admin"
+                            className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                        >
+                            <Volleyball className="h-5 w-5 transition-all group-hover:scale-110" />
+                            <span className="sr-only">Saeed's Shuffle</span>
+                        </Link>
+                    </div>
                     {navItems.map(item => (
                          <Link key={item.href} href={item.href} className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
                             <item.icon className="h-5 w-5" />
