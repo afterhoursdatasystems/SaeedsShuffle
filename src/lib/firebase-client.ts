3828a759488fba45f4200753c45d5fc03ece6503
Matt Taylor <matt@afterhoursds.com>
2025-09-19 16:41:27 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -1,16 +1,8 @@
 
 'use client';
 
-import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
-
-const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { firebaseConfig } from './firebase-config';
 
 // Client-side app initialization
 function initializeClientApp() {
---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import type { FirebaseOptions } from 'firebase/app';
+
+// This configuration is auto-generated and is safe to be exposed on the client-side.
+// Firebase security rules and App Check are used to protect your backend resources.
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "saeeds-shuffle.firebaseapp.com",
+  projectId: "saeeds-shuffle",
+  storageBucket: "saeeds-shuffle.appspot.com",
+  messagingSenderId: "1054734796349",
+  appId: "1:1054734796349:web:9107d6d538e1b3438e8d5f",
+  measurementId: "G-9X53MP813B"
+};
