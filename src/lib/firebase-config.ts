306257d132417b70180d264008f9bc652d76f967
Matt Taylor <matt@afterhoursds.com>
2025-09-19 16:46:48 +0000

    api key not valid. are you SURE there's nothing I need to do on the fire

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,11 @@
+import { type FirebaseOptions } from 'firebase/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "saeeds-shuffle.firebaseapp.com",
+  projectId: "saeeds-shuffle",
+  storageBucket: "saeeds-shuffle.appspot.com",
+  messagingSenderId: "1023532467402",
+  appId: "1:1023532467402:web:7a7c71f9859a5ac79668ef",
+  measurementId: "G-J5B83CS6S8"
+};
