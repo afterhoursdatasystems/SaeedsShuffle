b2a011afdee0516cdb5b0029ca170e9e0cda5dad
Matt Taylor <matt@afterhoursds.com>
2025-09-24 20:58:09 +0000

    There's still an issue with schedule generation. All Courts should have

--- src/components/schedule-generator.tsx
+++ src/components/schedule-generator.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import type { Match, GameFormat, GameVariant, Player, Team } from '@/types';
+import type { Match, Player, Team } from '@/types';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
